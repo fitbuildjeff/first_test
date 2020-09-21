20793e50502e9c754ef1b13c88f4ef466831f20d
Jeffrey Hanssen <fitbuildjeff@gmail.com>
2020-09-20 21:05:08 -0700

    trying to make an update

--- folder1/app.js
+++ folder1/app.js
@@ -1,30 +1,30 @@
 (function () {
-'use strict';
-  
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
-
-LunchCheckController.$inject = [$scope]
-function LunchCheckController($scope) {
-  $scope.message="";
-
-  $scope.sayMessage = function () {
-    var ingredients;
-
-    if ($scope.input) {
-      ingredients = $scope.input.split(",");
-      ingredients = items.filter(function(ingredient) {
-        return ingredient.length > 0;
-      });
-      if (ingredients.length <= 3) {
-        $scope.message = "Enjoy!";
+  'use strict';
+      
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+    
+  LunchCheckController.$inject = [$scope]
+  function LunchCheckController($scope) {
+    $scope.message="";
+    
+    $scope.sayMessage = function () {
+      var ingredients;
+    
+      if ($scope.input) {
+        ingredients = $scope.input.split(",");
+        ingredients = items.filter(function(ingredient) {
+          return ingredient.length > 0;
+        });
+        if (ingredients.length <= 3) {
+          $scope.message = "Enjoy!";
+        } else {
+          $scope.message = "Too much!";
+        }
       } else {
-        $scope.message = "Too much!";
+        $scope.message = "Please enter data first";
       }
-    } else {
-      $scope.message = "Please enter data first";
     }
-  }
-}  
-
-})();
+  }  
+    
+  })();
