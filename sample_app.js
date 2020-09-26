627f635db2f0b4c1f4b6ebc7cab849889de34095
Jeffrey Hanssen <fitbuildjeff@gmail.com>
2020-09-26 14:05:18 -0700

    trying to update

---
+++ sample_app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+    
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+  
+LunchCheckController.$inject = [$scope]
+function LunchCheckController($scope) {
+  $scope.message="";
+  
+  $scope.sayMessage = function () {
+    var ingredients;
+  
+    if ($scope.input) {
+      $scope.input = $scope.input.trim();
+      ingredients = $scope.input.split(",");
+      ingredients = items.filter(function(ingredient) {
+        return ingredient.length > 0;
+      });
+      if (ingredients.length <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+    } else {
+      $scope.message = "Please enter data first";
+    }
+  }
+}  
+  
+})();
